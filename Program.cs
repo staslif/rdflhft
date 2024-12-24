63511a0daae33068e6f2827bfcb1d8ccca139c48
staslif <149203375+staslif@users.noreply.github.com>
2024-12-24 10:45:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Diagnostics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        static void Main()
+        {
+            Random rand = new Random();
+            Console.WriteLine("введите кол-во элементов до 10000");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 1 || n > 10000)//проверка
+            {
+                Console.WriteLine("кол-во элементов  до 10000");
+                return;
+            }
+            int[] mass = new int[n];//массив
+            for (int i = 0; i < n; i++)//заполнение массива
+            {
+                mass[i] = rand.Next(1, 101);
+            }
+            int max = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 10; j < n; j++)
+                {
+                    int sum = mass[i] * mass[i] + mass[j] * mass[j];
+
+                    if (sum > max)
+                    {
+                        max = sum;
+                    }
+                }
+            }
+            Console.WriteLine("сумма квадратов = " + max);
+        }
+    }
+}
